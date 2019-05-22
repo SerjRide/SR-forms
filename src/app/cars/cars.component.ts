8b19facfa454442624649294ad2a8a06ce08f8ec
SerjRide <quicksquicks6@gmail.com>
2019-05-22 21:25:34 +0300

    less 9

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.scss']
 })
+
 export class CarsComponent {
 
   addCarStatus = '';
