4e19276d333db78a18b11603dcce0384c53ba360
SerjRide <quicksquicks6@gmail.com>
2019-05-22 03:20:01 +0300

    less 3

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+
+  // templateUrl: './car.component.html',
+
+  template: `
+
+    <h4>Car</h4>
+
+  `,
+
+  // styleUrls: ['./car.component.scss']
+
+  styles: [`
+
+    h4 {
+      color: aqua;
+    }
+
+  `]
+})
+export class CarComponent implements OnInit {
+
+  constructor() {
+
+
+
+  }
+
+  ngOnInit() {
+
+
+
+  }
+
+}
