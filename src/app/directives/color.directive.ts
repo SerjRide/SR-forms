dff3eb3fc82fbcdfe8318465b5171abd48c1f6fa
SerjRide <quicksquicks6@gmail.com>
2019-05-25 15:33:00 +0300

    less 21

--- src/app/directives/color.directive.ts
+++ src/app/directives/color.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef,
-         OnInit } from '@angular/core';
+         OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: "[appColor]"
@@ -7,10 +7,13 @@ import { Directive, ElementRef,
 
 export class ColorDirective implements OnInit {
 
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef,
+              private renderer: Renderer2) {}
 
   ngOnInit() {
+    const { nativeElement: el } = this.element;
+    this.renderer.setStyle(el, 'text-shadow','0 0 20px aqua');
+    this.renderer.addClass(el, 'directive-test');
     this.element.nativeElement.style.color = '#1A1A1A';
-    this.element.nativeElement.style.textShadow = '0 0 20px aqua';
   }
 }
